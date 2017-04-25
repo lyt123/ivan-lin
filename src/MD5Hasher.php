b81fded946cf4b38789a47c485bf25b6c3dfb819
lyt123 <1067081452@qq.com>
2017-04-25 20:13:24 +0800

    initial

---
+++ src/MD5Hasher.php
@@ -0,0 +1,19 @@
+<?php
+/* User:lyt123; Date:2017/4/20; QQ:1067081452 */
+
+namespace Laravist\Hasher;
+
+class MD5Hasher
+{
+    public function make($value, array $options = [])
+    {
+        $salt = isset($options['salt']) ? $options['salt'] : '';
+        return hash('md5', $value.$salt);
+    }
+
+    public function check($value, $hashValue, array $options = [])
+    {
+        $salt = isset($options['salt']) ? $options['salt'] : '';
+        return hash('md5', $value.$salt) === $hashValue;
+    }
+}
