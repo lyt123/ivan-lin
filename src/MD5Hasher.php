9570c81d52afec46492d9905fb40d334fb586275
lyt123 <1067081452@qq.com>
2017-04-26 10:19:28 +0800

    add travis

--- src/MD5Hasher.php
+++ src/MD5Hasher.php
@@ -15,7 +15,7 @@ class MD5Hasher
     public function check($value, $hashValue, array $options = [])
     {
         $salt = isset($options['salt']) ? $options['salt'] : '';
-        
+
         return hash('md5', $value . $salt) === $hashValue;
     }
 }
